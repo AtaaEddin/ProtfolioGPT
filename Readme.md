f7b0934719da595126a0f1ec26b3315b5228cb15
mouhamad ataa al daghstani <ataa_co@hotmail.com>
2023-06-10 19:46:30 +0300

    First commint

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# ProtfolioGPT
+
+Generate a Protfolio website using ChatGPT.
+
+### Generated example
+
+![Example](./Docs/example.png)
+
+## Running the application
+
+- Get ChatGPT API key.
+- Create 'keys.txt' file in the repo directory.
+- Run 'Python WebsiteGenerator.py'
+
+Note: Sometimes ChatAPT doesn't provide quality website, so you try few times to get a good one.
+
+## Conributions
+
+Conributions are welcome, you can send me a message or open an issue.
---
+++ contexts.py
@@ -0,0 +1,62 @@
+INIT_COMMANDS = [  
+{"role": "user", "content": 'Given text input from a user looking to do personal profile website help to build a full functional website (html,css,javascript) and try to use css libraries like tailwind or bootstrap, the website could contains personal profile image and an introduction and maybe history work and other important details that must be useful, start with creating the website without any introduction and answer only and ONLY WITH CODE. If you understand, say "OK."'},
+{"role": "assistant", "content": "OK."},
+{"role": "user", "content": '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My profile</title>
+</head>
+<style>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+
+    <!-- here goes the css of the website->
+</style>
+<body>
+    <!-- here goes the html of the website->
+    <!-- .... ->
+
+    <!-- here goes the menu bar ->
+    <!-- .... ->
+
+    <!-- here goes the personal introduction->
+    <!-- .... ->
+    
+    <!-- here goes the work experince ->
+    <!-- .... ->
+
+    <!-- here goes the hobbies->
+    <!-- .... ->
+
+    <!-- here goes other informations->
+    <!-- .... ->
+    <script>
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- here goes the javascript of the website->
+</script>
+</body>
+</html>
+
+please follow this EXACT website's structure and response ONLY with code. If you understand, say "OK."
+'''},
+{"role": "assistant", "content": "OK."},]
